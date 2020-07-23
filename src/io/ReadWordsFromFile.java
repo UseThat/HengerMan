f4dec19fb3fd395d05244088fd2f8a5ceef94c8d
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2020-07-23 08:44:15 +0200

    Create ReadWordsFromFile class

--- src/io/ReadWordsFromFile.java
+++ src/io/ReadWordsFromFile.java
@@ -1,10 +1,30 @@
 package io;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ReadWordsFromFile {
     private static String FILE_NAME = "passwords.txt";
     private BufferedReader bufferedReader;
+    private ArrayList<String> passwordsList;
 
+    public ArrayList<String> fileRead() throws IOException {
+        String readFile = "";
+        passwordsList = new ArrayList<>();
+        try{
+            bufferedReader = new BufferedReader(new FileReader(FILE_NAME));
+            while((readFile = bufferedReader.readLine()) != null){
+                passwordsList.add(readFile);
+            }
+        }catch(IOException ex){
+            System.out.println("Problem with file read");
+        }
+
+        System.out.println(passwordsList.toString());
+
+        return passwordsList;
+    }
 
 }
--- src/model/Passwords.java
+++ src/model/Passwords.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 
 public class Passwords {
     private ArrayList <String> passwords;
-    
 
+    public Passwords(ArrayList <String> passwords){
+        this.passwords = passwords;
+    }
+
+    public ArrayList<String> getPasswords() {
+        return passwords;
+    }
+
+    public void setPasswords(ArrayList<String> passwords) {
+        this.passwords = passwords;
+    }
 }
